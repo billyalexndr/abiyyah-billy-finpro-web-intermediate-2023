df1f661e320d5a70b39935b90568a5c3358a8390
Billy Alexander <billyalexndr@gmail.com>
2023-11-24 21:25:36 +0700

    feat: get api data

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from "react";
 
 const MovieList = () => {
@@ -9,7 +10,6 @@ const MovieList = () => {
                 "https://api.themoviedb.org/3/discover/movie?api_key=e64500ed35aa80d6aa47b51eaca63cbf"
             );
             const data = await response.json();
-            setMovies(data.result);
             console.log(data);
         } catch (error) {
             console.error("Error fetching movies:", error);
@@ -20,7 +20,20 @@ const MovieList = () => {
         fetchMovies();
     }, []);
 
-    return <></>;
+    return (
+        <div>
+            <h1>Daftar Film</h1>
+            <div className="movies-container">
+                {/* {movies.map((movie) => (
+                    <div key={movie.imdbID} className="movie-card">
+                        <h2>{movie.Title}</h2>
+                        <p>Year: {movie.Year}</p>
+                        <img src={movie.Poster} alt={movie.Title} />
+                    </div>
+                ))} */}
+            </div>
+        </div>
+    );
 };
 
 export default MovieList;
