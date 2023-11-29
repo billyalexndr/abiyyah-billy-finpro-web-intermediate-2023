a7faf20da733638210861e6517f9a3a2e5add6b3
Billy Alexander <billyalexndr@gmail.com>
2023-11-29 11:50:19 +0700

    (feat): successfully get data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,48 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import CardMovie from "@/components/CardMovie";
 
-const MovieList = () => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  const getMovies = async () => {
-      try {
-        const response = await axios.get(
-          "https://api.themoviedb.org/3/discover/movie?api_key=e64500ed35aa80d6aa47b51eaca63cbf"
-        );
-        setMovies(response.data.results);
-        console.log(response.data.results);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } 
-  };
-
-  return (
-    <div>
-      <h1>Daftar Film</h1>
-      <div className="movies-container">
-        {movies.map((movie) => (
-          <div key={movie.id} className="movie-card">
-            <h2>{movie.title}</h2>
-            <p>Release Date: {movie.release_date}</p>
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.title}
-            />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+interface Movie {
+    id: number;
+    title: string;
+    release_date: string;
+    poster_path: string;
 }
+
+const MovieList: React.FC = () => {
+    const [movies, setMovies] = useState<Movie[]>([]);
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    const getMovies = async () => {
+        try {
+            const response = await axios.get(
+                "https://api.themoviedb.org/3/discover/movie?api_key=e64500ed35aa80d6aa47b51eaca63cbf"
+            );
+            setMovies(response.data.results);
+            console.log(response.data.results);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    return (
+        <div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+                {movies.map((movie) => (
+                    <CardMovie
+                        key={movie.id}
+                        title={movie.title}
+                        description={`Release Date: ${movie.release_date}`}
+                        imageUrl={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default MovieList;
